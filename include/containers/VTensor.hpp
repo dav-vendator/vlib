2e7b4887e845242c5eb46f4485f06d68332ee298
davinder23031 <davinder23031@iiitd.ac.in>
2026-06-23 11:02:53 +0530

    VTensor: Added implementation for size, shape and stride

--- include/containers/VTensor.hpp
+++ include/containers/VTensor.hpp
@@ -10,56 +10,79 @@ Author: Dav Vendator
 #include <cstddef>
 #include <stdexcept>
 #include <vector>
-#include <algorithm>
-#include <numerics>
 
 namespace Vlib{
     template<typename T>
     class VTensor {
-    private:
-        T* data_;
-        std::vector<std::size_t> shape_;
-        std::vector<std::size_t> strides_;
-        std::size_t size_;
+            T* data_;
+            std::vector<std::size_t> shape_;
+            std::vector<std::size_t> strides_;
+            std::size_t size_;
 
-        void stride() noexcept;
+            void build_strides();
+            void build_size() noexcept;
 
-        void index(std::vector<std::size_t>&) noexcept const;
+            std::size_t offset(const std::vector<std::size_t>&) const noexcept;
 
-    public:
-        VTensor();
-        explicit VTensor(const std::vector<std::size_t>& shape);
-        VTensor(std::initializer_list<std::size_t> shape);
+        public:
+            VTensor();
+            explicit VTensor(const std::vector<std::size_t>& shape);
+            VTensor(std::initializer_list<std::size_t> shape);
 
-        ~VTensor();
+            ~VTensor();
 
-        VTensor(const VTensor& other);
-        VTensor& operator=(const Tensor& other);
+            VTensor(const VTensor& other);
+            VTensor& operator=(const Tensor& other);
 
-        VTensor(VTensor&& other) noexcept;
-        VTensor& operator=(VTensor&& other) noexcept;
+            VTensor(VTensor&& other) noexcept;
+            VTensor& operator=(VTensor&& other) noexcept;
 
-        std::size_t size() const noexcept;
-        std::size_t rank() const noexcept;
-        const std::vector<std::size_t>& shape() const noexcept;
-        const std::vector<std::size_t>& strides() const noexcept;
+            std::size_t size() const noexcept;
+            std::size_t rank() const noexcept;
+            const std::vector<std::size_t>& shape() const noexcept;
+            const std::vector<std::size_t>& strides() const noexcept;
 
-        T* data() noexcept;
-        const T* data() const noexcept;
+            T* data() noexcept;
+            const T* data() const noexcept;
 
-        T& operator()(std::initializer_list<std::size_t> indices);
-        const T& operator()(std::initializer_list<std::size_t> indices) const;
+            T& operator()(std::initializer_list<std::size_t> indices);
+            const T& operator()(std::initializer_list<std::size_t> indices) const;
 
-        void fill(const T& value);
+            void fill(const T& value);
 
-        void reshape(const std::vector<std::size_t>& new_shape);
-        void reshape(std::initializer_list<std::size_t> new_shape);
+            void reshape(const std::vector<std::size_t>& new_shape);
+            void reshape(std::initializer_list<std::size_t> new_shape);
 
-        void swap(VTensor& other) noexcept;
+            void swap(VTensor& other) noexcept;
     };
 
     template<typename T>
-    VTensor<T>::VTensor(): data_(nullptr),size_(0)
+    void VTensor<T>::build_size() noexcept {  
+        this->size_ = 1;
+        for (auto& val : this->shape_)
+            this->size_ *= val;
+    }
+
+    template<typename T>
+    void VTensor<T>::build_strides(){
+        this->strides_.resize(this->shape_.size());
+        std::size_t running = 1;
+        for (std::size_t i = this->shape_.size(); i-- > 0; ) {
+            this->strides_[i] = running;
+            running *= this->shape_[i];
+        }
+    }
+
+    template<typename T>
+    std::size_t VTensor<T>::offset(const std::vector<std::size_t>& indices) const noexcept{
+        std::size_t offset = 1;
+        for (std::size_t i = 0; i < indices.size(); i++)
+            offset += (this->strides_[i] * indices[[i]]);
+        return offset;
+    }
+
+    template<typename T>
+    VTensor<T>::VTensor(): data_(nullptr), size_(0)
     {}
 
     template<typename T>
